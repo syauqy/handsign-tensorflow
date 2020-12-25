075d2f45cdd9786ea8ecc47eb938a911f78c0d4d
syauqy <syauqyaziz@gmail.com>
2020-12-25 17:48:20 +0700

    add initial gesture model

--- src/emojimage/index.js
+++ src/emojimage/index.js
@@ -1,21 +1,55 @@
-import fingercrossed from './fingcrossed_emoji.png';
-import horns from './horns_emoji.png';
-import loveyou from './loveyou_emoji.png';
-import okay from './ok_emoji.png';
-import victory from './victory_emoji.png';
-import vulcan from './vulcan_emoji.png';
-import heart from './heart_emoji.png';
-import thumbsup from './thumbs_up_emoji.png';
+import fingercrossed_moji from './fingcrossed_emoji.png';
+import horns_moji from './horns_emoji.png';
+import loveyou_moji from './loveyou_emoji.png';
+import okay_moji from './ok_emoji.png';
+import victory_moji from './victory_emoji.png';
+import vulcan_moji from './vulcan_emoji.png';
+import heart_moji from './heart_emoji.png';
+import thumbsup_moji from './thumbs_up_emoji.png';
 
-const Emojimage =  {
-    thumbs_up: thumbsup,
-    victory: victory,
-    love: heart,
-    vulcan: vulcan,
-    fingercrossed: fingercrossed,
-    horns: horns,
-    love_you: loveyou,
-    okay: okay
+export const Emojimage =  {
+    thumbs_up: thumbsup_moji,
+    victory: victory_moji,
+    love: heart_moji,
+    vulcan: vulcan_moji,
+    fingercrossed: fingercrossed_moji,
+    horns: horns_moji,
+    love_you: loveyou_moji,
+    okay: okay_moji
 }
 
-export default Emojimage;
+export const Emojipass =  [
+    {
+        src: thumbsup_moji,
+        alt: 'thumbs_up'
+    },
+    {
+        src: victory_moji,
+        alt: 'victory'
+    },
+    {
+        src: heart_moji,
+        alt: 'love'
+    },
+    {
+        src: vulcan_moji,
+        alt: 'vulcan'
+    },
+    {
+        src: fingercrossed_moji,
+        alt: 'fingercrossed'
+    },
+    {
+        src: horns_moji,
+        alt: 'horns'
+    },
+    {
+        src: loveyou_moji,
+        alt: 'love_you'
+    },
+    {
+        src: okay_moji,
+        alt: 'okay'
+    },
+]
+    
--- src/pages/handsign.js
+++ src/pages/handsign.js
@@ -15,10 +15,11 @@ import Webcam from 'react-webcam';
 import {drawHand} from '../components/handposeutil';
 import * as fp from 'fingerpose';
 
-import Emojis from '../gestures'; 
+import Emojis from '../gestures';
 
-import Emojimage from '../emojimage';
+import {Emojimage, Emojipass} from '../emojimage';
 
+import '../styles/App.css'
 
 import '@tensorflow/tfjs-backend-webgl';
 
@@ -26,20 +27,56 @@ export default function Handsign() {
     const webcamRef = useRef(null);
     const canvasRef = useRef(null);
     const [camState,
-        setCamState] = useState("off");
+        setCamState] = useState("on");
+    const [gameState, setGameState] = useState(null);
 
-    const [emoji, setEmoji] = useState(null);
+    const [emoji,
+        setEmoji] = useState(null);
 
 
     async function runHandpose() {
-        setCamState('on');
+        // setCamState('on');
         const net = await handpose.load();
         console.log("Handpose model loaded.");
+        _emojiList();
         //  Loop and detect hands
         setInterval(() => {
             detect(net);
         }, 100);
     };
+
+    let currentEmoji = 0;
+
+    function generateEmojis() {
+        const password = shuffle(Emojipass);
+        const passwordContainer = document.getElementById('emojis');
+
+        passwordContainer.innerHTML = '';
+
+        password.forEach(obj => {
+            const img = document.createElement('img');
+            img.src = obj.src;
+            img.alt = obj.alt;
+
+            passwordContainer.appendChild(img);
+        });
+
+        return password;
+    }
+
+    let emojiList;
+
+    function _emojiList() {
+        emojiList = generateEmojis();
+    }
+
+    function shuffle(a) {
+        for (let i = a.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [a[i], a[j]] = [a[j], a[i]];
+        }
+        return a;
+    }
 
     async function detect(net) {
 
@@ -60,37 +97,52 @@ export default function Handsign() {
 
             // Make Detections
             const hand = await net.estimateHands(video);
-            // console.log(hand);
-
-            // console.log(Emojis);
 
             if (hand.length > 0) {
                 // add "✌🏻" and "👍" as sample gestures
-                const GE = new fp.GestureEstimator([fp.Gestures.VictoryGesture,
-                  fp.Gestures.ThumbsUpGesture, 
-                  Emojis.loveGesture, Emojis.vulcanGesture, Emojis.fingerCrossedGesture, Emojis.loveYouGesture, Emojis.hornsGesture,
-                Emojis.okayGesture]);
+                const GE = new fp.GestureEstimator([
+                    fp.Gestures.VictoryGesture,
+                    fp.Gestures.ThumbsUpGesture,
+                    Emojis.loveGesture,
+                    Emojis.vulcanGesture,
+                    Emojis.fingerCrossedGesture,
+                    Emojis.loveYouGesture,
+                    Emojis.hornsGesture,
+                    Emojis.okayGesture
+                ]);
 
                 const estimatedGestures = await GE.estimate(hand[0].landmarks, 6.5);
                 console.log(estimatedGestures);
 
-                  document.querySelector('.pose-data').innerHTML = JSON.stringify(estimatedGestures.poseData, null, 2);
+                document
+                    .querySelector('.pose-data')
+                    .innerHTML = JSON.stringify(estimatedGestures.poseData, null, 2);
 
-                
-                if(estimatedGestures.gestures !== undefined && estimatedGestures.gestures.length > 0){
-                  const confidence = estimatedGestures.gestures.map(
-                    (p) => p.confidence
-                  );
-                  const maxConfidence = confidence.indexOf(
-                    Math.max.apply(undefined, confidence)
-                  );
-                  setEmoji(estimatedGestures.gestures[maxConfidence].name);
-                  console.log(emoji);
+                if (currentEmoji === emojiList.length) {
+                    document
+                        .querySelector('.pose-data')
+                        .innerText = "Berhasil!!";
+                    return;
                 }
-                
-            }
 
-            
+                if (estimatedGestures.gestures !== undefined && estimatedGestures.gestures.length > 0) {
+                    const confidence = estimatedGestures
+                        .gestures
+                        .map((p) => p.confidence);
+                    const maxConfidence = confidence.indexOf(Math.max.apply(undefined, confidence));
+
+                    console.log('gestures', estimatedGestures.gestures);
+                    console.log('emoji', emojiList[currentEmoji].alt);
+                    // const match = estimatedGestures.find(g => emojiList[currentEmoji].alt === g.gestures.name);
+                    if(emojiList[currentEmoji].alt === estimatedGestures.gestures[maxConfidence].name){
+                              document.querySelector(`[alt=${estimatedGestures.gestures[maxConfidence].name}]`).classList.add('found');
+                              currentEmoji++;
+                            }
+                    setEmoji(estimatedGestures.gestures[maxConfidence].name);
+                    console.log(emoji);
+                }
+
+            }
 
             // Draw mesh
             const ctx = canvasRef
@@ -103,62 +155,78 @@ export default function Handsign() {
     // useEffect(()=>{runHandpose()},[]);
 
     function turnOffCamera() {
+      if(camState === "on"){
         setCamState('off');
+      }else{
+        setCamState('on');
+      }
+        
     }
 
     return (
         <div>
+            <div className="responsive-embed">
+            
+            <div id="webcam-container">
             {camState === 'on'
                 ? <Webcam
                         ref={webcamRef}
                         style={{
-                        position: "absolute",
-                        marginLeft: "auto",
-                        marginRight: "auto",
-                        left: 0,
-                        right: 0,
+                        position: "relative",
+                        // marginLeft: "auto",
+                        // marginRight: "auto",
+                        // left: 0,
+                        // right: 0,
                         textAlign: "center",
-                        zindex: 9,
-                        width: 640,
-                        height: 480
-                    }}/>
+                        // zindex: 9,
+                        width: '480px',
+                        height: '800px'
+                    }}>
+                      
+                    </Webcam>
                 : <div>
                     No cam
                 </div>}
 
-            <canvas
+                <div id="emojis">emojis</div>
+
+                <canvas
                 ref={canvasRef}
                 style={{
                 position: "absolute",
-                marginLeft: "auto",
-                marginRight: "auto",
+                margin: "auto",
+                top: 0,
+                bottom: 0,
+                // marginLeft: "auto",
+                // marginRight: "auto",
                 left: 0,
                 right: 0,
                 textAlign: "center",
                 zindex: 9,
-                width: 640,
-                height: 480
+                width: '480px',
+                height: '400px'
             }}/>
 
-{emoji !== null || 'undefined' ? (
-          <img
-            src={Emojimage[emoji]}
-            style={{
-              position: "absolute",
-              marginLeft: "auto",
-              marginRight: "auto",
-              left: 400,
-              bottom: 500,
-              right: 0,
-              textAlign: "center",
-              height: 100,
-            }}
-          />
-        ) : (
-          " "
-        )}
-        <pre className="pose-data"></pre>
-
+             {emoji !== null || 'undefined'
+                ? (<img
+                    src={Emojimage[emoji]}
+                    style={{
+                    position: "absolute",
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    left: 400,
+                    bottom: 500,
+                    right: 0,
+                    textAlign: "center",
+                    height: 50
+                }}/>)
+                : (" ")}
+            </div>
+            </div>
+            
+            
+            <pre className="pose-data"></pre>
+            <button id="generateEmoji" onClick={_emojiList}>Generate emojis</button>
             <button onClick={turnOffCamera}>matiin kamera</button>
             <button onClick={runHandpose}>run handpose</button>
 
